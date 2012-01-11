2830044df503ceb853ba67b3de1c45ab5ead5754
Ethan J. Brown <ethan.j.brown@gmail.com>
2012-01-11 15:09:42 -0500

    put together some readere tests to ensure a couple of things
    - that the counter sample read is cancellable
    - that the right number of samples come back
    - that the data returned is basically correct for a few counters (% proc time and context switches/sec) -- this is a pretty inexact science -- maybe better to just check for non-zero values?

---
+++ tests/PerfTap.Tests/Counter/PerfmonCounterReaderTests.cs
@@ -0,0 +1,70 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PerfmonCounterReaderTests.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace PerfTap.Counter.Tests
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics;
+	using System.Linq;
+	using System.Threading;
+	using Xunit;
+	using Xunit.Extensions;
+
+	/// <summary>
+	/// TODO: Update summary.
+	/// </summary>
+	public class PerfmonCounterReaderTests
+	{
+		[Fact]
+		public void GetCounterSamples_IsCancellable()
+		{
+			var reader = new PerfmonCounterReader();
+			using (var cancellationTokenSource = new CancellationTokenSource())
+			{
+				List<PerformanceCounterSampleSet> sampleSets = new List<PerformanceCounterSampleSet>();
+				foreach (var sampleSet in reader.GetCounterSamples(TimeSpan.FromSeconds(5), 20, cancellationTokenSource.Token))
+				{
+					sampleSets.Add(sampleSet);
+					cancellationTokenSource.Cancel();
+				}
+				Assert.Single(sampleSets);
+			}
+		}
+
+		[Fact]
+		public void GetCounterSamples_ReturnsExpectedSampleCount()
+		{
+			var reader = new PerfmonCounterReader();
+			using (var cancellationTokenSource = new CancellationTokenSource())
+			{
+				Assert.Single(reader.GetCounterSamples(new[] { @"\processor(_total)\% processor time" }, TimeSpan.FromSeconds(1), 1, cancellationTokenSource.Token)
+					.SelectMany(set => set.CounterSamples));
+			}
+		}
+
+		[Theory]
+		[InlineData(@"\processor(_total)\% processor time", PerformanceCounterType.Timer100NsInverse, 1, 100, "_total", 10000000UL)]
+		[InlineData(@"\System\Context Switches/sec", PerformanceCounterType.RateOfCountsPerSecond32, 1000, 10000, null, 3579545UL)]
+		public void GetCounterSamples_ProcessorTime_HasReasonableValues(string counter, PerformanceCounterType counterType, double minValue, double maxValue, string instanceName, ulong timeBase)
+		{
+			var reader = new PerfmonCounterReader();
+			using (var cancellationTokenSource = new CancellationTokenSource())
+			{
+				var sample = reader.GetCounterSamples(new[] { counter }, TimeSpan.FromSeconds(1), 1, cancellationTokenSource.Token)
+					.SelectMany(set => set.CounterSamples)
+					.First();
+
+				//TODO: this is bad form - replace with an object comparer 
+				Assert.Equal(counterType, sample.CounterType);
+				Assert.InRange(sample.CookedValue, minValue, maxValue);
+				Assert.Equal(instanceName, sample.InstanceName);
+				Assert.Equal(timeBase, sample.TimeBase);
+				Assert.True(sample.Path.ToLower().EndsWith(counter.ToLower()));
+			}
+		}
+	}
+}
